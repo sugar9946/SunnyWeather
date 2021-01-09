51449c0ad3b081d8eef25293e2bd0a614e618c47
sugar9946 <994698396@qq.com>
2021-01-09 16:00:05 +0800

    项目完成

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.java
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.java
@@ -7,7 +7,7 @@ import android.content.Context;
 public class SunnyWeatherApplication extends Application {
 
 
-    public static  final String TOKEN = "2714yLjZKii71qVg";
+    public static  final String TOKEN = "flJ1K8iHWin3s2xk";
     @SuppressLint("StaticFieldLeak")
     static public Context context;
     @Override
